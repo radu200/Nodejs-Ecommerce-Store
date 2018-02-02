9f54ea9d79cef618f53785666265c4dd7e06caa8
radu200 <raduprodan200@gmail.com>
2018-02-02 23:02:53 +0000

    added quatity button

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -10331,14 +10331,21 @@ return jQuery;
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__scss_main_scss__ = __webpack_require__(2);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__scss_main_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__scss_main_scss__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__main_js__ = __webpack_require__(3);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__main_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__main_js__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__delete_product_js__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__delete_product_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__delete_product_js__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__navbar_js__ = __webpack_require__(4);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__navbar_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2__navbar_js__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__image_js__ = __webpack_require__(5);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__image_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3__image_js__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__image_src_js__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__image_src_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3__image_src_js__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__partials_quantity_btn_js__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__partials_quantity_btn_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4__partials_quantity_btn_js__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__partials_category_btn_js__ = __webpack_require__(7);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__partials_category_btn_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5__partials_category_btn_js__);
 
-// import $ from "jquery";
+//scss files
+
+//js files
+
 
 
 
@@ -10383,40 +10390,9 @@ $(document).ready(function () {
 
 /***/ }),
 /* 4 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-/* WEBPACK VAR INJECTION */(function(jQuery) {
-jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
-jQuery('.quantity').each(function () {
-  var spinner = jQuery(this),
-      input = spinner.find('input[type="number"]'),
-      btnUp = spinner.find('.quantity-up'),
-      btnDown = spinner.find('.quantity-down'),
-      min = input.attr('min'),
-      max = input.attr('max');
 
-  btnUp.click(function () {
-    var oldValue = parseFloat(input.val());
-    if (oldValue >= max) {
-      var newVal = oldValue;
-    } else {
-      var newVal = oldValue + 1;
-    }
-    spinner.find("input").val(newVal);
-    spinner.find("input").trigger("change");
-  });
-
-  btnDown.click(function () {
-    var oldValue = parseFloat(input.val());
-    if (oldValue <= min) {
-      var newVal = oldValue;
-    } else {
-      var newVal = oldValue - 1;
-    }
-    spinner.find("input").val(newVal);
-    spinner.find("input").trigger("change");
-  });
-});
 let openSide = document.getElementById('open-slide');
 if (openSide !== null) {
 
@@ -10431,7 +10407,6 @@ if (closeSide !== null) {
     document.getElementById('side-menu').style.width = '0';
   });
 }
-/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
 /* 5 */
@@ -10446,6 +10421,87 @@ if (closeSide !== null) {
     });
 });
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+var x, i, j, selElmnt, a, b, c;
+/*look for any elements with the class "custom-select":*/
+x = document.getElementsByClassName("custom-select");
+for (i = 0; i < x.length; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  /*for each element, create a new DIV that will act as the selected item:*/
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /*for each element, create a new DIV that will contain the option list:*/
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < selElmnt.length; j++) {
+    /*for each option in the original select element,
+    create a new DIV that will act as an option item:*/
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function (e) {
+      /*when an item is clicked, update the original select box,
+      and the selected item:*/
+      var i, s, h;
+      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+      h = this.parentNode.previousSibling;
+      for (i = 0; i < s.length; i++) {
+        if (s.options[i].innerHTML == this.innerHTML) {
+          s.selectedIndex = i;
+          h.innerHTML = this.innerHTML;
+          break;
+        }
+      }
+      h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function (e) {
+    /*when the select box is clicked, close any other select boxes,
+    and open/close the current select box:*/
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+function closeAllSelect(elmnt) {
+  /*a function that will close all select boxes in the document,
+  except the current select box:*/
+  var x,
+      y,
+      i,
+      arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  for (i = 0; i < y.length; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i);
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < x.length; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports) {
+
+
 
 /***/ })
 /******/ ]);
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 
-// import $ from "jquery";
+//scss files
 import css from '../scss/main.scss';
-import DeleteReq from "./main.js";
+//js files
+import DeleteReq from "./delete_product.js";
 import NavBar from "./navbar.js";
-import image from "./image.js";
+import image from "./image_src.js";
+import quantity_btn from "./partials/quantity_btn.js";
+import category_btn from "./partials/category_btn.js"
